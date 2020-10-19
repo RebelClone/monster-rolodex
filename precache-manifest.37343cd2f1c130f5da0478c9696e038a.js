87ad6d74aabb14d8a590462cfdd720607f0d511e
Rebel Clone <rebelclone505@gmail.com>
2020-10-19 19:15:06 +0530

    Updates

---
+++ precache-manifest.37343cd2f1c130f5da0478c9696e038a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "105c53f1044ae7c0a1fab27c36370e3f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "53004bbd8f4acfb8d341",
+    "url": "/monsters-rolodex/static/css/main.15491beb.chunk.css"
+  },
+  {
+    "revision": "2d19631dba197ad7891e",
+    "url": "/monsters-rolodex/static/js/2.e1627e68.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.e1627e68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53004bbd8f4acfb8d341",
+    "url": "/monsters-rolodex/static/js/main.17e87daf.chunk.js"
+  },
+  {
+    "revision": "dfba82531f669c6858b0",
+    "url": "/monsters-rolodex/static/js/runtime-main.589ddcef.js"
+  }
+]);
